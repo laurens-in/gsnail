05838a13aa51ba4e9933cb4f016241c9f8d10e23
laurens <laurens.inauen@hkb.bfh.ch>
2022-05-01 03:47:31 +0200

    update page yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import logo from "./logo.svg";
-import snail from "./assets/snail.png";
+import snail from "./assets/snail_creep.gif";
+import { features } from "./data/features";
 import "./App.css";
 
 function App() {
@@ -10,8 +11,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={snail}></img>
-        <p>this site is under construction...</p>
+        <h1>
+          don't use <s>gmail</s>, be a <b>gsnail</b>!
+        </h1>
+        <button>Sign up!</button>
       </header>
+      {features.map((feature, index) => (
+        <div
+          className={"feature" + (index % 2 === 0 ? "" : " odd")}
+          key={index}
+        >
+          <div className="featureText">
+            <h1>{feature.title}</h1>
+            <p>{feature.description}</p>
+          </div>
+          <img className="featureImg" src={feature.img}></img>
+        </div>
+      ))}
+      <footer>
+        <p>gsnail™ is a trademark owned by Gslug Inc.</p>
+      </footer>
     </div>
   );
 }
---
+++ src/data/features.ts
@@ -0,0 +1,24 @@
+import ad from "../assets/ad.png";
+import eye from "../assets/eye.png";
+import key from "../assets/key.png";
+
+export const features: Feature[] = [
+  {
+    title: "Privacy",
+    description:
+      "Your data is safely stored in swiss data centers. Switzerland has a long history of working with international security agencies to make sure everyone is safe. While we have very average privacy laws, we are not afraid to look the other way, should they get in the way of making the world safer for you.",
+    img: eye,
+  },
+  {
+    title: "Ads",
+    description:
+      "Want to move away from other mail providers, but don't want to miss out on the advantages of targeted ads? We got you covered! We provide all your data to our partners so that they know exactly what ads to show you, that's what we call a win-win!",
+    img: ad,
+  },
+  {
+    title: "Security",
+    description:
+      "While most mail providers use OpenPGP, we are proud to go one step further. With Open™PGP technology, even your private keys are public! This guarantees us access to all your private conversations in case we need it, because our moto is 'if you have nothing to hide, don't worry about it'.",
+    img: key,
+  },
+];
